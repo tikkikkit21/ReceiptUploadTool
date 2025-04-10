74e1a155ed95f6bf2e3ae0a58b0193fe2af71d8b
tikkikkit21 <tikkikkit21@gmail.com>
2025-04-10 15:58:58 -0400

    Create initial receipt controller

---
+++ backend/Controllers/ReceiptsController.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReceiptAPI.Models;
+
+namespace ReceiptAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiptsController : ControllerBase
+    {
+        private readonly Context _context;
+
+        public ReceiptsController(Context context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Receipts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Receipt>>> GetReceipts()
+        {
+            if (_context.Receipts == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Receipts.ToListAsync();
+        }
+
+        // GET: api/Receipts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Receipt>> GetReceipt(long id)
+        {
+            if (_context.Receipts == null)
+            {
+                return NotFound();
+            }
+
+            var receipt = await _context.Receipts.FindAsync(id);
+            if (receipt == null)
+            {
+                return NotFound();
+            }
+
+            return receipt;
+        }
+
+        // POST: api/Receipts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Receipt>> PostReceipt(Receipt receipt)
+        {
+            if (_context.Receipts == null)
+            {
+                return Problem("Receipt list is null, unable to add receipt");
+            }
+
+            _context.Receipts.Add(receipt);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetReceipt", new { id = receipt.Id }, receipt);
+        }
+    }
+}
